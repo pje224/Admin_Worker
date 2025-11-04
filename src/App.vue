d5c8f35c8e6473946ef796606d90c961378fd7bb
unknown <pje224@naver.com>
2025-11-04 12:35:06 +0900

    언어변경하기

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- 헤더 -->
+    <Header_w :isDark="isDarkHeader" :logoSrc="isDarkHeader ?  '/images/favicon_192.png':'/images/link.png' " />
+    <!-- <Header_w :isDark="isDarkHeader" :logoSrc="isDarkHeader ?  '/images/favicon_192.png':'/images/link.png' " /> -->
+    <!-- 페이지 내용 -->
+    <router-view></router-view>
+    <QuickTop />
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+import Header_w from "./components/Header_w.vue";
+import QuickTop from "./components/QuickTop.vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const isDarkHeader = computed(() => ["ReserVue", "ReviewVue"].includes(route.name));
+</script>
+
+<style lang="scss" scoped></style>
